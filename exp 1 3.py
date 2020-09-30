763d648fb79333bee7107606081b79c357d89196
Rishu Kumar <57801239+rkDevguru47@users.noreply.github.com>
2020-09-30 22:31:43 +0530

    Add files via upload

---
+++ exp 1 3.py
@@ -0,0 +1,13 @@
+# A Joint Effort by CYBERpunk Netrunners ( Group 15 )
+#written by: Rishu Kumar
+
+#Write a Personal Greeter Program
+#The Personal Greeter program adds a single, but very cool,
+#element to the Greeter program: user input. Instead of working
+#with a predefined value, the computer lets the user enter his or
+#her name and then uses it to say hi.
+
+#to input the user's name
+usr_name=input("\n Please Enter your NAME \n")
+#to print greeting and message
+print("\n HI ",usr_name,"! HOW are you ? \n")
